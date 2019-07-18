d5d0e680c9627a249ee6f5c7e9a2d35248da1afe
takara1356 <goblin7283@gmail.com>
2019-07-18 23:07:05 +0900

    editの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新したよ"
   end
 
   def create
