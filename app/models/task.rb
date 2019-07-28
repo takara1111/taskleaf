8524605e52bd379f760b9c3d9c222a29b1bedbef
takara1356 <goblin7283@gmail.com>
2019-07-28 17:07:47 +0900

    オリジナルValidation追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_conma
+
+    private
+    def validate_name_not_including_conma
+      errors.add(:name, 'にカンマを含めることはできないっすよ' if name&.include?(','))
+    end
 end
