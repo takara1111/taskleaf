b32f7c41f059ad9189cbda8b9306e9e5606bfb80
takara1356 <goblin7283@gmail.com>
2019-08-03 22:22:05 +0900

    コールバック関数の実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+  # コールバック関数の呼び出し
+  # before_validation :set_nameless_name
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_conma
@@ -7,4 +10,8 @@ class Task < ApplicationRecord
     def validate_name_not_including_conma
       errors.add(:name, 'にカンマを含めることはできないっすよ' if name&.include?(','))
     end
+
+    # def set_nameless_name
+    #   self.name = '名前なし' if name.blank?
+    # end
 end
