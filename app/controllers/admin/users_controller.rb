8808e83d2d33ee18b108aa40b628d54ce45e48f5
takara1356 <goblin7283@gmail.com>
2019-08-04 13:19:20 +0900

    ユーザー管理機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,18 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -13,6 +25,22 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」さんを登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」さんを削除しました。"
+  end
+  
     private
     
     def user_params
