edfb51a15c159959f81ba373e15ac1f62221f825
takara1356 <goblin7283@gmail.com>
2019-08-06 21:12:45 +0900

    タスク取得メソッドを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
     # ログインしているユーザー以外が覗こうとしても見つからない
-    @task = current_user.tasks.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -13,19 +15,20 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    # before_actionで@task取得
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
+    # task = current_user.tasks.find(params[:id])
+    @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新したよ"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除したよ"
+    # task = current_user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除したよ"
   end
 
   def create
@@ -48,5 +51,9 @@ class TasksController < ApplicationController
 
     def task_params
       params.require(:task).permit(:name, :description)
+    end
+
+    def set_task
+      @task = current_user.tasks.find(params[:id])
     end
 end
