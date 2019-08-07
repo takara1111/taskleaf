f4287b03865c3a37cc2cfc2650d79e4672f51d6c
takara1356 <goblin7283@gmail.com>
2019-08-07 22:46:03 +0900

    shared_examplesの実装

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,30 +5,31 @@ describe 'タスク管理機能', type: :system do
   let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
   let(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
-  describe '一覧表示機能' do
-    before do
-      # 作成者がユーザーAであるタスクを作成しておく
-      visit login_path(task_a)
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
+  before do
+    # 作成者がユーザーAであるタスクを作成しておく
+    visit login_path(task_a)
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク'}
+  end
+
+  describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
 
-      context 'ユーザーBがログインしているとき' do
-        let(:login_user) { user_b }
-        
-        it 'ユーザーAが作成したタスクが表示されない' do
-          # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
-          expect(page).to have_no_content '最初のタスク'
-        end
+    context 'ユーザーBがログインしているとき' do
+      let(:login_user) { user_b }
+      
+      it 'ユーザーAが作成したタスクが表示されない' do
+        # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+        expect(page).to have_no_content '最初のタスク'
       end
     end
 
@@ -40,9 +41,7 @@ describe 'タスク管理機能', type: :system do
           visit task_path(task_a)
         end
 
-        it 'ユーザーAが作成したタスクが表示される' do
-          expect(page).to have_content '最初のタスク'
-        end
+        it_behaves_like 'ユーザーAが作成したタスクが表示される'
       end
 
       
